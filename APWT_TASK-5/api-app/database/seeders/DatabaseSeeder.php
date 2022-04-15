d49b688460642284fe9269ac6a316ea067a4b721
nafi-rahman <nafirahman16@gmail.com>
2022-04-16 02:56:46 +0600

    a
    a

---
+++ APWT_TASK-5/api-app/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comment;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    Comment::factory()
+      ->times(3)
+      ->create();
+
+    }
+}
